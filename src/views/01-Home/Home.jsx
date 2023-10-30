74b64e0449e7e21edc5fbc0debb3271d1ab4b0f7
CoralJM <info.coraljimenez@gmail.com>
2023-10-30 10:26:19 +0100

    Animation skills

--- src/views/01-Home/Home.jsx
+++ src/views/01-Home/Home.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
+import { Container } from "react-bootstrap";
 
 export const Home = () => {
-    return (
-        <img className="banner" src="../../public/young_lab.png"></img>
-    )
-}
+  const [items, setItems] = useState([
+    "WEB DESIGN",
+    "BRAND IDENTITY",
+    "COPYWRITING",
+    "PHOTO & VIDEO PRODUCTION",
+    "ART DIRECTION",
+    "DEVELOPMENT",
+    "LET'S TALK ABOUT CIBERSECURITY",
+  ]);
+
+  useEffect(() => {
+    setItems((prevItems) => [...prevItems, ...prevItems]);
+  }, []);
+
+  return (
+    <Container>
+      <img className="banner" src="../../public/young_lab.png"></img>
+
+      <div className="skills-animation-container">
+        <div className="skills-animation">
+          {items.map((text, index) => (
+            <div className="animation-text" key={index}>
+              {text}
+            </div>
+          ))}
+        </div>
+      </div>
+      
+    </Container>
+  );
+};
